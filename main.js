4d8fceab2aea5b8229264ff50514e67fd7b54f00
Andressa Costa <79861793+and0994@users.noreply.github.com>
2021-07-23 15:51:42 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+function consultaCep() {
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json";
+    console.log(cep);
+    //FUNÇAO DO JQUERY 
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response) {
+            $("#logradouro").html(response.logradouro); //JQUERY
+            console.log(response); //Posso colocar após o . o que quero mostrar
+            //  document.getElementById("logradouro").innerHTML = response.logradouro;
+            document.getElementById("bairro").innerHTML = response.bairro;
+            document.getElementById("localidade").innerHTML = response.localidade;
+            document.getElementById("uf").innerHTML = response.uf;
+
+        }
+    })
+}
