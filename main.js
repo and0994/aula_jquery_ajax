89d3244ac8bb406616558d2c4d52b9ea32f55b6e
Andressa Costa <79861793+and0994@users.noreply.github.com>
2021-07-28 14:41:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,18 +2,27 @@ function consultaCep() {
     var cep = document.getElementById("cep").value;
     var url = "https://viacep.com.br/ws/" + cep + "/json";
     console.log(cep);
-    //FUNÇAO DO JQUERY 
+    //FUNÇAO DO JQUERY
     $.ajax({
         url: url,
         type: "GET",
         success: function(response) {
-            $("#logradouro").html(response.logradouro); //JQUERY
+            // $("#logradouro").html(response.logradouro); //JQUERY
             console.log(response); //Posso colocar após o . o que quero mostrar
             //  document.getElementById("logradouro").innerHTML = response.logradouro;
-            document.getElementById("bairro").innerHTML = response.bairro;
-            document.getElementById("localidade").innerHTML = response.localidade;
-            document.getElementById("uf").innerHTML = response.uf;
-
+            /*  document.getElementById("bairro").innerHTML = response.bairro;
+              document.getElementById("localidade").innerHTML = response.localidade;
+              document.getElementById("uf").innerHTML = response.uf;*/
+            $("#tituloCep").html("CEP " + response.cep);
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $(".cep").show();
         }
     })
 }
+$(function() {
+
+    $(".cep").hide(); //Serve para esconder os elementos
+})
